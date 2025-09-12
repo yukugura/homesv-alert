ec7bd50286d50120b16cdfae91140711c7a9512b
yukugura <yukugura@gmail.com>
2025-09-13 03:54:44 +0900

    initial

---
+++ main.py
@@ -0,0 +1,98 @@
+import os
+import time
+import asyncio
+from ping3 import ping
+from dotenv import load_dotenv
+from gpiozero import OutputDevice
+
+# Load .env
+load_dotenv()
+HOSTS_STR = os.getenv("HOSTS")
+CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL"))
+HOSTS = [host.strip() for host in HOSTS_STR.split(",")] # カンマ区切りで複数のホストをリストに変換＆空白除去
+
+# GPIO define
+RELAY01 = 2
+RELAY02 = 3
+
+# Relay define
+relay01 = OutputDevice(RELAY01, active_high=False)
+relay02 = OutputDevice(RELAY02, active_high=False)
+
+# hostがアクティブかどうかを確認する関数
+async def is_active(host):
+    try:
+        # pingコマンドを実行
+        response_time = await asyncio.to_thread(ping, host, timeout=1)
+        return response_time is not False and response_time is not None
+    except Exception as e:
+        print(f"pingエラー: {host} - {e}")
+        return False
+
+# 指定したhostにpingを送信し失敗した場合、リトライする関数
+async def is_host_up_with_retry(host, max_retry=5):
+    for i in range(max_retry + 1):
+        if await is_active(host):
+            # ping成功時
+            return True
+        else:
+            # ping失敗時
+            if i < max_retry:
+                await asyncio.sleep(1)  # 1秒待ってからリトライ
+    return False
+
+# リレー制御関数
+def control_relay(state):
+    if state == 'green':
+        relay01.off()
+        relay02.off()
+    elif state == 'yellow':
+        relay01.on()
+        relay02.off()
+    elif state == 'red':
+        relay01.on()
+        relay02.on()
+    else:
+        print("Invalid state. Use 'green', 'yellow', or 'red'.")
+        return False
+    return True
+
+# メイン関数
+async def main():
+    # 機器を監視するメインループ
+    print("ネットワーク監視プログラムを開始します。Ctrl + Cで停止します。")
+    print(f"監視対象ホスト: {', '.join(HOSTS)}")
+    try:
+        while True:
+            tasks = [is_host_up_with_retry(host) for host in HOSTS]
+            results = await asyncio.gather(*tasks)
+
+            # 状態判定
+            all_ok = all(results)
+            any_down = not all_ok and any(not res for res in results)
+
+            if all_ok:
+                # すべてのホストがアクティブな場合
+                control_relay('green')
+            elif any_down:
+                # いずれかのホストが非アクティブな場合
+                control_relay('red')
+                down_hosts = [HOSTS[i] for i, res in enumerate(results) if not res]
+                print(f"いくつかのホストがダウンしています: {', '.join(down_hosts)}")
+            else:
+                # すべてのホストが非アクティブな場合
+                control_relay('red')
+                print("すべてのホストがダウンしています！")
+            await asyncio.sleep(CHECK_INTERVAL)
+
+    except asyncio.CancelledError:
+        print("ネットワーク監視プログラムを停止します。")
+        control_relay('yellow')  # 停止時にリレーをOFFにする
+    finally:
+        control_relay('green')  # 停止時にリレーをOFFにする
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("プログラムがユーザーによって中断されました。")
